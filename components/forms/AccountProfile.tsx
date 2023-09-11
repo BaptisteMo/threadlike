5bd6c05e4a6e4fed2e2ded5e0e5ce971e9e6abf6
BaptisteMo <morillon.bapt@gmail.com>
2023-09-11 16:40:51 +0200

    implémentation de uploadthing et des fonction onSubmit pour la photo de profil

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,9 @@ import {zodResolver} from "@hookform/resolvers/zod"
 import { UserValidation } from '@/lib/validations/user';
 import * as z from 'zod';
 import Image from 'next/image';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing'
+
 
 interface Props {
     user:{
@@ -34,6 +37,7 @@ interface Props {
 export const AccountProfile = ({user, btnTitle}: Props) => {
 
     const [files, setFiles] = useState <File[]>([]);
+    const { startUpload } = useUploadThing("media");
 
     const form = useForm({
         resolver : zodResolver(UserValidation),
@@ -59,13 +63,23 @@ export const AccountProfile = ({user, btnTitle}: Props) => {
                 fieldChange(imageDataUrl);
             }
             fileReader.readAsDataURL(file);
+
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async  (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo; 
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged){
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes [0].url){
+                values.profile_photo = imgRes[0].url;
+            }
+        }
+
+        // TODO : Update user profile
       }
     
     
