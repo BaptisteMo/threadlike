46c6db3947afd714d080dd71fd01c7081078b302
BaptisteMo <morillon.bapt@gmail.com>
2023-09-11 16:17:53 +0200

    Allow user to add ang change PP

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { Button } from "@/components/ui/button"
 import {
@@ -33,6 +33,8 @@ interface Props {
 
 export const AccountProfile = ({user, btnTitle}: Props) => {
 
+    const [files, setFiles] = useState <File[]>([]);
+
     const form = useForm({
         resolver : zodResolver(UserValidation),
         defaultValues : {
@@ -43,8 +45,21 @@ export const AccountProfile = ({user, btnTitle}: Props) => {
         },
     });
 
-    const handleImage = (e  : ChangeEvent, fieldChange : (value : string) => void ) => {
+    const handleImage = (e  : ChangeEvent<HTMLInputElement>, fieldChange : (value : string) => void ) => {
         e.preventDefault ();
+        const fileReader = new FileReader();
+
+        if(e.target.files &&  e.target.files.length > 0){
+            const file = e.target.files[0]
+
+            setFiles(Array.from(e.target.files));
+            if(!file.type.includes('image')) return;
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || '';
+                fieldChange(imageDataUrl);
+            }
+            fileReader.readAsDataURL(file);
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
