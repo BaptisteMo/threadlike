794149cde34993d98a58371ed507a63fde89c987
BaptisteMo <morillon.bapt@gmail.com>
2023-09-12 15:28:20 +0200

    On a maintenant l'affichage des différents threads sur le page d'accueil, chacuns est lié à une page spéficique sur laquelle on pourra répondre

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -4,6 +4,7 @@ import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { revalidatePath } from "next/cache";
+import { skip } from "node:test";
 
 interface Params {
 
@@ -33,4 +34,36 @@ export async function createThread({ text, author, communityId, path }: Params){
  }catch (error : any){
     throw new Error (`Errore creating thread : ${error.message}`)
  }
+}
+
+
+export async function fetchPosts(pageNumber= 1, pageSize : 20) {
+    connectToDB();
+    // On calcul le nombre de poste à passer pour passer à la page suivante
+
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    // Récupérer uniquement les threads de top level, pas les commentaires
+    const postsQuery = Thread.find({ parenId : {$in: [null, undefined]}})
+        .sort({ createdAt : 'desc'})
+        .skip (skipAmount)
+        .limit (pageSize)
+        .populate({path:'author', model: User})
+        .populate ({ 
+            path: 'children',
+            populate: {
+                path: 'author',
+                model : User,
+                select: "_id name parentId image", 
+            }
+    })
+    const totalPostsCount = await Thread.countDocuments({ parenId : {$in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return {
+        posts,
+        isNext
+    }
 }
