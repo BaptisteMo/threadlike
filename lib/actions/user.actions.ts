2ce7a22c06e53a98b90126458aa83c7562df8036
BaptisteMo <morillon.bapt@gmail.com>
2023-09-18 13:43:00 +0200

    Création de la page activité

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -153,3 +153,33 @@ export async function fetchUsers({
         throw new Error (`Erreur pour récupérer les users : ${error.message} `)
     }
 }
+
+export async function getActivity(userId : string){
+
+    try{
+        connectToDB();
+
+        //find all thread created by user 
+        const userThreads = await Thread.find ({ author : userId})
+
+        // Find all the child thread ids -> Les réponses depuis le champs 'children' de chaque thread récupéré
+        const childThreadIds = userThreads.reduce((acc, userThread)=> {
+            return acc.concat(userThread.children);
+        }, [])
+
+        const replies = await Thread.find({
+            _id : {$in : childThreadIds},
+            author : {$ne : userId}
+        }).populate({
+            path : 'author',
+            model : User,
+            select : 'name image _id',
+        })
+
+        return replies; 
+
+    }catch (error : any){
+        throw new Error (`Erreur pour récupérer les activités : ${error.message} `)    
+
+    }
+}
